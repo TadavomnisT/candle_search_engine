42adf4951a45e02b69b03c8cedc66f01a4efe415
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2022-01-21 12:04:06 +0330

    Add back-end API
    Add back-end API , implemented with PHP.

---
+++ front-end/Lucene_API.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+class Lucene_API
+{
+
+    public function index(array $directories = [] , array $files = [] , array $links = [] )
+    {
+        $json = json_encode( 
+            [
+                "directories" => $directories,
+                "files" => $files,
+                "links" => $links
+            ]
+        );
+        $command =  "cd ../back-end/Java/ &&  php API_wrapper.php -index \"" . str_replace( "\"" , "\\\"" , $json ) . "\" && cd ../../front-end";
+        $result = shell_exec( $command );
+        return $result ;
+    }
+
+    public function query( string $query = "" )
+    {
+        $command =  "cd ../back-end/Java/ &&  php API_wrapper.php -query \"" . str_replace( "\"" , "\\\"" , $query ) . "\" && cd ../../front-end";
+        $result = shell_exec( $command );
+        return $result ;
+    }
+
+    
+}
+
+
+
+?>
