15e9b6397f3ca381c91579cc7afff523a225709f
behrad.b <63199745+TadavomnisT@users.noreply.github.com>
2022-02-14 13:07:04 +0330

    Starting to code a crawler
    Starting to code a crawler.

---
+++ web-crawler/Web_crawler.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+class Web_crawler
+{
+
+    public function __construct( $black_list = [] )
+    {
+        $this->black_list = $black_list;
+    }
+
+    public function crawl_url(String $url , $depth = 0)
+    {
+        while (true) {
+            # code...
+        }
+    }
+    
+}
+
+
+
+
+?>
